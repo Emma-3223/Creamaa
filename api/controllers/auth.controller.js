ca2120bd70c8624ab1fded9e28b8df5375be8d19
Emma-90844 <enabojohn@gmail.com>
2025-05-09 16:13:45 +0300

    Added Controller register and login functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,29 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body
-    // HASH PASSWORD
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // CREATE NEW USER
-    const newUser = await pris
+    try {
+        // HASH PASSWORD
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // CREATE NEW USER
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            }
+        })
 
+        console.log(newUser)
+        res.status(201).json({ message: 'User Created Successfully' })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Failed to Create User' })
+    }
 
 }
-
-
-
+// LOGIN
 export const login = (req, res) => {
 
 }
